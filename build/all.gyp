8301b96c0331d18296ea169b384e429e68208897
lianliuwei <lianliuwei@gmail.com>
2013-07-10 16:45:48 +0800

    add build/

---
+++ build/all.gyp
@@ -0,0 +1,11 @@
+{
+  'targets': [
+    {
+      'target_name': 'all',
+      'type': 'none',
+      'dependencies': [
+        '../device/device.gyp:*',
+      ],
+    }, # target_name: All
+  ], # conditions
+}
---
+++ build/pack.bat
@@ -0,0 +1,9 @@
+rd /Q /S osd
+mkdir osd
+copy Release\\osd.exe osd
+copy Release\\LOGO.bmp osd
+copy Release\\lite.zh osd
+copy Release\\elite.zh osd
+copy Release\\base.dll osd
+copy Release\\base_ex.dll osd
+copy Release\\ToolkitPro1521vc90U.dll osd
