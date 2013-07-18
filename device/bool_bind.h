d01d01ed543df85f4410d074ecf2d0b8e92eb334
lianliuwei <lianliuwei@gmail.com>
2013-07-18 10:50:01 +0800

    bool_bind no use template to config. so register config can be in one place.

--- device/bool_bind.h
+++ device/bool_bind.h
@@ -4,11 +4,15 @@
 #include "base/logging.h"
 
 // bind to Register Memory bit.
-template<int index, bool invert>
+// bacause use memory ptr, so Bind can no copy.
 class BoolBind {
 public:
-  BoolBind(char* ptr) 
-      : ptr_((uint8*)(ptr)) {
+  static const int kByteSize = 1;
+
+  BoolBind(uint8* ptr, int index, bool no_invert) 
+      : ptr_(ptr)
+      , index_(index)
+      , invert_(!no_invert) {
     DCHECK(index >= 0 && index <= 7);
     DCHECK(ptr);
   }
@@ -16,41 +20,27 @@ public:
   ~BoolBind() {}
 
   bool value() const {
-    bool ret = ((*ptr_) & (1 << index)) != 0;
-    return invert ? !ret : ret;
+    bool ret = ((*ptr_) & (1 << index_)) != 0;
+    return invert_ ? !ret : ret;
   }
  
   void set_value(bool value) {
-    if (invert)
+    if (invert_)
       value = !value;
     if (value) 
-      *ptr_ |= (1 << index);
+      *ptr_ |= (1 << index_);
     else
-      *ptr_ &= ~(1 << index);
+      *ptr_ &= ~(1 << index_);
   }
 
-  void Toggle(int index) {
-    *ptr_ ^= (1 << index);
+  void Toggle() {
+    *ptr_ ^= (1 << index_);
   }
 
 private:
   uint8* ptr_;
+  int index_;
+  bool invert_;
+
+  DISALLOW_COPY_AND_ASSIGN(BoolBind);
 };
-
-
-typedef BoolBind<0, false> BoolBind0;
-typedef BoolBind<0, true> BoolBind0Invert;
-typedef BoolBind<1, false> BoolBind1;
-typedef BoolBind<1, true> BoolBind1Invert;
-typedef BoolBind<2, false> BoolBind2;
-typedef BoolBind<2, true> BoolBind2Invert;
-typedef BoolBind<3, false> BoolBind3;
-typedef BoolBind<3, true> BoolBind3Invert;
-typedef BoolBind<4, false> BoolBind4;
-typedef BoolBind<4, true> BoolBind4Invert;
-typedef BoolBind<5, false> BoolBind5;
-typedef BoolBind<5, true> BoolBind5Invert;
-typedef BoolBind<6, false> BoolBind6;
-typedef BoolBind<6, true> BoolBind6Invert;
-typedef BoolBind<7, false> BoolBind7;
-typedef BoolBind<7, true> BoolBind7Invert;
--- device/bool_bind_unittest.cc
+++ device/bool_bind_unittest.cc
@@ -3,58 +3,75 @@
 #include "testing/gtest/include/gtest/gtest.h"
 
 TEST(BoolBindTest, TestBoolBind0) {
-  char memory = 0x0;
-  BoolBind0 bind0(&memory);
+  uint8 memory = 0x0;
+  BoolBind bind0(&memory, 0, true);
   EXPECT_EQ(bind0.value(), false);
   bind0.set_value(true);
   EXPECT_EQ(bind0.value(), true);
-  EXPECT_EQ(0x1, static_cast<uint8>(memory));
+  EXPECT_EQ(0x1, memory);
   bind0.set_value(false);
   EXPECT_EQ(bind0.value(), false);
-  EXPECT_EQ(0x0, static_cast<uint8>(memory));
+  EXPECT_EQ(0x0, memory);
+}
+
+TEST(BoolBindTest, TestToggle) {
+  uint8 memory = 0x0;
+  BoolBind bind0(&memory, 0, true);
+  EXPECT_EQ(bind0.value(), false);
+  bind0.set_value(true);
+  EXPECT_EQ(bind0.value(), true);
+  EXPECT_EQ(0x1, memory);
+  bind0.Toggle();
+  EXPECT_EQ(bind0.value(), false);
+  EXPECT_EQ(0x0, memory);
 }
 
 TEST(BoolBindTest, TestBoolBind0Invert) {
-  char memory = 0x0;
-  BoolBind0Invert bind0(&memory);
+  uint8 memory = 0x0;
+  BoolBind bind0(&memory, 0, false);
   EXPECT_EQ(bind0.value(), true);
   bind0.set_value(false);
   EXPECT_EQ(bind0.value(), false);
-  EXPECT_EQ(0x1, static_cast<uint8>(memory));
+  EXPECT_EQ(0x1, memory);
   bind0.set_value(true);
   EXPECT_EQ(bind0.value(), true);
-  EXPECT_EQ(0x0, static_cast<uint8>(memory));
+  EXPECT_EQ(0x0, memory);
+}
+
+TEST(BoolBindTest, TestByteSize) {
+  EXPECT_EQ(BoolBind::kByteSize, 1);
 }
 
 TEST(BoolBindTest, TestBoolBind7) {
-  char memory = 0x0;
-  BoolBind7 bind7(&memory);
+  uint8 memory = 0x0;
+  BoolBind bind7(&memory, 7, true);
   EXPECT_EQ(bind7.value(), false);
   bind7.set_value(true);
   EXPECT_EQ(bind7.value(), true);
-  EXPECT_EQ(0x80, static_cast<uint8>(memory));
+  EXPECT_EQ(0x80, memory);
   bind7.set_value(false);
   EXPECT_EQ(bind7.value(), false);
-  EXPECT_EQ(0x0, static_cast<uint8>(memory));
+  EXPECT_EQ(0x0, memory);
 }
 
 TEST(BoolBindTest, TestBoolBind7Invert) {
-  char memory = 0x0;
-  BoolBind7Invert bind7(&memory);
+    uint8 memory = 0x0;
+  BoolBind bind7(&memory, 7, false);
   EXPECT_EQ(bind7.value(), true);
   bind7.set_value(false);
   EXPECT_EQ(bind7.value(), false);
-  EXPECT_EQ(0x80, static_cast<uint8>(memory));
+  EXPECT_EQ(0x80, memory);
   bind7.set_value(true);
   EXPECT_EQ(bind7.value(), true);
-  EXPECT_EQ(0x0, static_cast<uint8>(memory));
+  EXPECT_EQ(0x0, memory);
 }
 
 TEST(BoolBindTest, TestBindTwo) {
-  char memory = 0x0;
-  BoolBind0 bind0(&memory);
-  BoolBind5 bind5(&memory);
-  BoolBind7 bind7(&memory);
+  uint8 memory = 0x0;
+  BoolBind bind0(&memory, 0, true);
+  BoolBind bind5(&memory, 5, true);
+  BoolBind bind7(&memory, 7, true);
+
   EXPECT_EQ(bind0.value(), false);
   EXPECT_EQ(bind5.value(), false);
   EXPECT_EQ(bind7.value(), false);
@@ -62,30 +79,30 @@ TEST(BoolBindTest, TestBindTwo) {
   EXPECT_EQ(bind0.value(), true);
   EXPECT_EQ(bind5.value(), false);
   EXPECT_EQ(bind7.value(), false);
-  EXPECT_EQ(0x1, static_cast<uint8>(memory));
+  EXPECT_EQ(0x1, memory);
   bind5.set_value(true);
   EXPECT_EQ(bind0.value(), true);
   EXPECT_EQ(bind5.value(), true);
   EXPECT_EQ(bind7.value(), false);
-  EXPECT_EQ(0x21, static_cast<uint8>(memory));
+  EXPECT_EQ(0x21, memory);
   bind7.set_value(true);
   EXPECT_EQ(bind0.value(), true);
   EXPECT_EQ(bind5.value(), true);
   EXPECT_EQ(bind7.value(), true);
-  EXPECT_EQ(0xA1, static_cast<uint8>(memory));
+  EXPECT_EQ(0xA1, memory);
   bind0.set_value(false);
   EXPECT_EQ(bind0.value(), false);
   EXPECT_EQ(bind5.value(), true);
   EXPECT_EQ(bind7.value(), true);
-  EXPECT_EQ(0xA0, static_cast<uint8>(memory));
+  EXPECT_EQ(0xA0, memory);
   bind5.set_value(false);
   EXPECT_EQ(bind0.value(), false);
   EXPECT_EQ(bind5.value(), false);
   EXPECT_EQ(bind7.value(), true);
-  EXPECT_EQ(0x80, static_cast<uint8>(memory));
+  EXPECT_EQ(0x80, memory);
   bind7.set_value(false);
   EXPECT_EQ(bind0.value(), false);
   EXPECT_EQ(bind5.value(), false);
   EXPECT_EQ(bind7.value(), false);
-  EXPECT_EQ(0x00, static_cast<uint8>(memory));
+  EXPECT_EQ(0x00, memory);
 }
