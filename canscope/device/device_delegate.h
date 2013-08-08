3f974e4313b74b4ac85cc1dd0cc1283cbc841df7
lianliuwei <lianliuwei@gmail.com>
2013-08-08 16:12:05 +0800

    add GetDeviceInfo() function.

--- canscope/device/device_delegate.h
+++ canscope/device/device_delegate.h
@@ -2,16 +2,21 @@
 
 #include "base/basictypes.h"
 
+namespace canscope {
+class DeviceInfo;
+
 // set to hardware
 class DeviceDelegate {
 public:
   // FPGA or arm control
   virtual bool WriteDevice(uint32 addr, uint8* buffer, int size) = 0;
   virtual bool ReadDevice(uint32 addr, uint8* buffer, int size) = 0;
-
+  virtual bool GetDeviceInfo(DeviceInfo* device_info) = 0;
   // I2C
 
 protected:
   DeviceDelegate() {}
   virtual ~DeviceDelegate() {}
 };
+
+} // namespace canscope
