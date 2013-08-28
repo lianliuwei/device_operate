b13efc5a8bf9b33f2c66a6e1a3ea37060337cbd2
lianliuwei <lianliuwei@gmail.com>
2013-08-28 17:10:16 +0800

    add canscope_device_property_constants.h define osc property key.

---
+++ canscope/device/canscope_device_property_constants.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace canscope {
+// Osc Device
+static const char* kOscDeviceGroup = "OscDevice";
+static const char* kRangeCANHItem = "CANH.Range";
+static const char* kOffsetCANHItem = "CANH.Offset";
+static const char* kCouplingCANHItem = "CANH.Coupling";
+static const char* kRangeCANLItem = "CANL.RangeCANH";
+static const char* kOffsetCANLItem = "CANL.OffsetCANH";
+static const char* kCouplingCANLItem = "CANL.Coupling";
+static const char* kRangeCANDIFFItem = "CANDIFF.Range";
+static const char* kOffsetCANDIFFItem = "CANDIFF.Offset";
+static const char* kDiffCtrlItem = "DiffCtrl";
+static const char* kTimeBaseItem = "Time.Base";
+static const char* kTimeOffsetItem = "Time.Offset";
+static const char* kAutoTimeItem = "Trigger.AutoTime";
+static const char* kTriggerSourceItem = "Trigger.Source";
+static const char* kTriggerTypeItem = "Trigger.Type";
+static const char* kTriggerModeItem = "Trigger.Mode";
+static const char* kTriggerSensItem = "Trigger.Sens";
+static const char* kCompareItem = "Trigger.Compare";
+static const char* kTriggerVoltItem = "Trigger.Volt";
+static const char* kTimeParamItem = "Trigger.Param";
+}; // namespace canscope
