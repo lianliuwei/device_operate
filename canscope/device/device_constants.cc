426c0cb05f1b515e5c4b96fe94a10eca12e616ee
lianliuwei <lianliuwei@gmail.com>
2013-09-25 16:39:02 +0800

    add device_constants

---
+++ canscope/device/device_constants.cc
@@ -0,0 +1,26 @@
+#include "canscope/device/device_constants.h"
+
+namespace canscope {
+
+// OscDevice
+const char kOscDevice[]                = "OscDevice";
+const char kOscCANHVoltRange[]         = "CANH.Range";
+const char kOscCANHOffset[]            = "CANH.Offset";
+const char kOscCANHCoupling[]          = "CANH.Coupling";
+const char kOscCANLVoltRange[]         = "CANL.Range";
+const char kOscCANLOffset[]            = "CANL.Offset";
+const char kOscCANLCoupling[]          = "CANL.Coupling";
+const char kOscCANDIFFVoltRange[]      = "CANDIFF.Range";
+const char kOscCANDIFFOffset[]         = "CANDIFF.Offset";
+const char kOscDiffCtrl[]              = "DiffCtrl";
+const char kOscTimeBase[]              = "Time.Base";
+const char kOscTimeOffset[]            = "Time.Offset";
+const char kOscAutoTime[]              = "Trigger.AutoTime";
+const char kOscTriggerSource[]         = "Trigger.Source";
+const char kOscTriggerType[]           = "Trigger.Type";
+const char kOscTriggerMode[]           = "Trigger.Mode";
+const char kOscTriggerSens[]           = "Trigger.Sens";
+const char kOscCompare[]               = "Trigger.Compare";
+const char kOscTriggerVolt[]           = "Trigger.Volt";
+const char kOscTimeParam[]             = "TimeParam";
+} // namespace canscope
