288a4d0c5e1e5d5c64a8028c4bfd7c9d08e55f08
lianliuwei <lianliuwei@gmail.com>
2013-09-27 15:06:38 +0800

    enhance GetValueTest add SetValueTest

--- canscope/device/canscope_device_manager_unittest.cc
+++ canscope/device/canscope_device_manager_unittest.cc
@@ -1,5 +1,7 @@
 #include "base/message_loop.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/json/json_string_value_serializer.h"
+#include "base/json/json_reader.h"
 
 #include "common/common_thread_manager.h"
 #include "common/common_thread.h"
@@ -10,10 +12,55 @@
 #include "canscope/device/canscope_device_manager.h"
 #include "canscope/device/canscope_device_manager_handle.h"
 #include "canscope/canscope_notification_types.h"
+#include "canscope/device/device_thread_mock.h"
 
 using namespace base;
 using namespace common;
 using namespace canscope;
+
+using testing::Invoke;
+
+namespace {
+static const char kOscConfig [] =  {" \
+{ \
+  \"CANH.Range\" : 3, \
+  \"CANH.Offset\" : 0.0, \
+  \"CANH.Coupling\" : 1, \
+  \"CANL.Range\" : 3, \
+  \"CANL.Offset\" : 0.0, \
+  \"CANL.Coupling\" : 1, \
+  \"CANDIFF.Range\" : 3, \
+  \"CANDIFF.Offset\" : 0.0, \
+  \"DiffCtrl\" : 0, \
+  \"Time.Base\" : 4, \
+  \"Time.Offset\" : 0.0, \
+  \"Trigger.AutoTime\" : 100.0, \
+  \"Trigger.Source\" : 2, \
+  \"Trigger.Type\" : 0, \
+  \"Trigger.Mode\" : 0, \
+  \"Trigger.Sens\" : 0, \
+  \"Trigger.Compare\" : 0, \
+  \"Trigger.Volt\" : 0.0, \
+  \"TimeParam\" : 1.0 \
+} \
+"};
+
+DictionaryValue* GetDefaultConfig() {
+  std::string content(kOscConfig);
+  JSONStringValueSerializer serializer(content);
+  Value* value = serializer.Deserialize(NULL, NULL);
+  EXPECT_TRUE(NULL != value);
+  EXPECT_TRUE(value->IsType(Value::TYPE_DICTIONARY));
+  DictionaryValue* dict_value;
+  value->GetAsDictionary(&dict_value);
+  return dict_value;
+}
+
+bool ActionIsDeviceThread() {
+  return CommonThread::CurrentlyOn(CommonThread::DEVICE);
+}
+
+}
 
 class TestProcess : public common::CommonThreadManager {
 public:
@@ -51,12 +98,16 @@ public:
   virtual ~CANScopeDeviceManagerTest() {}
 
   static void SetUpTestCase() {
+    ON_CALL(g_DeviceThreadMock.Get(), 
+        IsDeviceThread()).WillByDefault(Invoke(&ActionIsDeviceThread));
     new TestProcess();
     GetTestProcess()->Init();
   }
   static void TearDownTestCase() {
     DestroyTestProcess();
   }
+
+  void GetValueTest();
 
 private:
   virtual void SetUp() {
@@ -73,7 +124,10 @@ private:
   }
 
   void CreateDeviceManager() {
-    CANScopeDeviceManager::Create();
+    CANScopeDeviceManager* manager = CANScopeDeviceManager::Create();
+    manager->osc_device_config_test()->Update(GetDefaultConfig());
+    manager->osc_device()->InitFromConfig();
+    manager->osc_device()->Init();
     event_.Signal();
   }
 
@@ -85,14 +139,47 @@ private:
     }
   }
 
-
   WaitableEvent event_;
   NotificationRegistrar registrar_;
 };
 
-TEST_F(CANScopeDeviceManagerTest, GetValue) {
+void CANScopeDeviceManagerTest::GetValueTest() {
+  OscDeviceHandle* handle = CANScopeDeviceManagerHandle::GetInstance()->
+    osc_device_handle();
 
-  CommonThread::PostTask(CommonThread::DEVICE, FROM_HERE, 
-      Bind(&DeviceManager::StartDestroying, CANScopeDeviceManager::GetInstance()));
+  EXPECT_EQ(k8V, handle->volt_range_can_h.value());
+  EXPECT_DOUBLE_EQ(0.0, handle->offset_can_h.value());
+  EXPECT_EQ(kAC, handle->coupling_can_h.value());
+  EXPECT_EQ(k8V, handle->volt_range_can_l.value());
+  EXPECT_DOUBLE_EQ(0.0, handle->offset_can_l.value());
+  EXPECT_EQ(kAC, handle->coupling_can_l.value());
+  EXPECT_EQ(kSub, handle->diff_ctrl.value());
+  EXPECT_EQ(k20us, handle->time_base.value());
+  EXPECT_DOUBLE_EQ(0.0, handle->time_offset.value());
+  EXPECT_DOUBLE_EQ(100, handle->auto_time.value());
+  EXPECT_EQ(kTriggerSourceCANDIFF, handle->trigger_source.value());
+  EXPECT_EQ(kRisingEdge, handle->trigger_type.value());
+  EXPECT_EQ(kAuto, handle->trigger_mode.value());
+  EXPECT_EQ(kDefault, handle->trigger_sens.value());
+  EXPECT_EQ(kGreater, handle->compare.value());
+  EXPECT_DOUBLE_EQ(0.0, handle->trigger_volt.value());
+  EXPECT_DOUBLE_EQ(1.0, handle->time_param.value());
+
+  CANScopeDeviceManager::GetInstance()->StartDestroying();
+}
+
+TEST_F(CANScopeDeviceManagerTest, GetValue) {
+  CommonThread::PostTask(CommonThread::UI, FROM_HERE, 
+      Bind(&CANScopeDeviceManagerTest::GetValueTest, Unretained(this)));
+  GetTestProcess()->MainMessageLoopRun();
+}
+
+TEST_F(CANScopeDeviceManagerTest, SetValue) {
+  OscDeviceHandle* handle = CANScopeDeviceManagerHandle::GetInstance()->
+      osc_device_handle();
+  EXPECT_EQ(k8V, handle->volt_range_can_h.value());
+  handle->volt_range_can_h.set_value(k1V);
+  EXPECT_EQ(k1V, handle->volt_range_can_h.value());
+  CANScopeDeviceManager::GetInstance()->StartDestroying();
   GetTestProcess()->MainMessageLoopRun();
 }
