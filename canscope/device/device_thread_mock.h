da5fb47a41533f4b35e0782673d403862bcf0def
lianliuwei <lianliuwei@gmail.com>
2013-09-27 18:03:01 +0800

    add ctor dtor

--- canscope/device/device_thread_mock.h
+++ canscope/device/device_thread_mock.h
@@ -6,7 +6,13 @@
 
 class DeviceThreadMock {
 public:
+  DeviceThreadMock() {}
+  ~DeviceThreadMock() {}
+
   MOCK_METHOD0(IsDeviceThread, bool());
+
+private:
+  DISALLOW_COPY_AND_ASSIGN(DeviceThreadMock);
 };
 
 extern base::LazyInstance<DeviceThreadMock> g_DeviceThreadMock;
