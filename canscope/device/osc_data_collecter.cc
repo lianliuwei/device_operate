b97d26a72e533797c52ad1f555620d04acc7ba83
lianliuwei <lianliuwei@gmail.com>
2014-02-10 14:54:15 +0800

    implement osc_data_collecter as state machine manage trigger state inside

--- canscope/device/osc_data_collecter.cc
+++ canscope/device/osc_data_collecter.cc
@@ -1,21 +1,27 @@
 #include "canscope/device/osc_data_collecter.h"
 
+#include "canscope/device/canscope_device_constants.h"
+#include "canscope/device/usb_port_device_delegate.h"
+#include "canscope/device/register/scope_ctrl_register.h"
+#include "canscope/device/register/canscope_device_register_constants.h"
+
 using namespace canscope::device;
+using namespace device;
+
 namespace canscope {
 
-LoopState OscDataCollecter::OnLoopRun() {
+DataCollecter::LoopState OscDataCollecter::OnLoopRun() {
   CHECK_NE(STATE_NONE, next_state_);
-  Error rv = OK;
   LoopState loop_state = STOP;
   {
     State state = next_state_;
     next_state_ = STATE_NONE;
     switch (state) {
       case STATE_LOAD_CALIBRATE:
-        rv = DoLoadCalibrate(&loop_state);
+        DoLoadCalibrate(&loop_state);
         break;
       case STATE_COLLECT:
-        rv = DoCollect(&loop_state);
+        DoCollect(&loop_state);
         break;
       default:
         NOTREACHED();
@@ -37,8 +43,9 @@ void OscDataCollecter::DoLoadCalibrate(LoopState* loop_state) {
   return;
 }
 
-void OscDataCollecter::SaveError() {
-
+void OscDataCollecter::SaveError(Error error) {
+  rv_ = error;
+  // TODO may be save current stack
 }
 
 void OscDataCollecter::DeviceOffine() {
@@ -47,44 +54,106 @@ void OscDataCollecter::DeviceOffine() {
   set_stop_by_offine(true);
 }
 
-#define CHECK_DEVICE() \
+#define CHECK_DEVICE(error) \
 do { \
-  canscope::device::Error error = LastDeviceError(); \
-  if (error != OK) { \
-    SaveError(); \
+  if ((error) != OK) { \
+    SaveError((error)); \
     DeviceOffine(); \
     return; \
   } \
 } while (0)
 
+// TODO collect algorithm is wrong, is depend on trigger config
+// add a new state for long time wait for trigger trig : AutoTime is large
 void OscDataCollecter::DoCollect(LoopState* loop_state) {
   *loop_state = STOP;
 
-  osc_device_->Start();
-  CHECK_DEVICE();
+  Error error = OK;
+  error = StartScope();
+  CHECK_DEVICE(error);
   for (int i = 0; i < kOscCollectUpdateStateCount; ++i) {
-    osc_device_->UpdateTriggerState();
-    CHECK_DEVICE();
-    if (osc_device_->IsCollected())
+    error = UpdateTriggerState();
+    CHECK_DEVICE(error);
+    if (IsCollected())
       break;
-    if (i == kOscCollectUpdateStateCount) {
+    if (i == kOscCollectUpdateStateCount - 1) {
       DeviceOffine();
       return;
     }
-    SleepMilliseconds(kOscCollectUpdateStateInterval);
+    SleepMs(kOscCollectUpdateStateInterval);
   }
-  OscRawDataHandle raw_data(type_, LastConfig());
-  memset(raw_data->data(), raw_data->size());
-  osc_device_->ReadData(raw_data->data(), raw_data->size());
-  CHECK_DEVICE();
+  OscRawDataHandle raw_data = 
+      new OscRawData(osc_device_->device_type(), LastConfig());
+
+  error = ReadData(raw_data);
+  CHECK_DEVICE(error);
   
+  if (!call_back_.is_null()) {
+    call_back_.Run(raw_data);
+  }
+
   next_state_ = STATE_COLLECT;
   *loop_state = !IsSingle() ? NEXT_LOOP : STOP;
-
-  return OK;
 }
 
 #undef CHECK_DEVICE
 
+bool OscDataCollecter::WriteDevice(RegisterMemory* memory) {
+  return device_delegate_->WriteDevice(
+    memory->start_addr(), memory->buffer(), memory->size());
+}
+
+bool OscDataCollecter::ReadDevice(RegisterMemory* memory) {
+  return device_delegate_->ReadDevice(
+    memory->start_addr(), memory->buffer(), memory->size());
+}
+
+Error OscDataCollecter::ReadData(OscRawDataHandle raw_data) {
+  memset(raw_data->data(), 0, raw_data->size());
+  bool ret = usb_port()->ReadEP3(kScopeReadAddr, 
+      kUsbModelStream, raw_data->data(), raw_data->size());
+  return ret ? OK : ERR_READ_DEVICE;
+}
+
+Error OscDataCollecter::UpdateTriggerState() {
+  bool ret = ReadDevice(&(trigger_state_.memory));
+  return ret ? OK : ERR_READ_DEVICE;
+}
+
+Error OscDataCollecter::StartScope() {
+  ScopeCtrlRegister scope_ctrl;
+  scope_ctrl.scope_ctrl.set_value(true);
+  bool ret = WriteDevice(&(scope_ctrl.memory));
+  return ret ? OK : ERR_WRITE_DEVICE;
+}
+
+bool OscDataCollecter::IsCollected() {
+  return trigger_state_.clet_bit.value();
+}
+
+OscRawDataDeviceConfigHandle OscDataCollecter::LastConfig() {
+  ConfigManager::Config config = osc_device_->config_manager()->GetLast();
+  if (last_config_.get() == NULL) {
+    last_config_ = new OscRawDataDeviceConfig(config);
+  } else if (!last_config_->SameConfig(config)) {
+    last_config_ = new OscRawDataDeviceConfig(config);
+  }
+  return last_config_;
+}
+
+OscDataCollecter::OscDataCollecter(DataCollectedCallback call_back, 
+                                   UsbPortDeviceDelegate* device_delegate, 
+                                   OscDevice* osc_device)
+    : call_back_(call_back)
+    , device_delegate_(device_delegate)
+    , osc_device_(osc_device)
+    , next_state_(STATE_LOAD_CALIBRATE)
+    , rv_(OK) {
+  SetFreq(20);
+}
+
+UsbPort* OscDataCollecter::usb_port() {
+  return device_delegate_->usb_port_ptr();
+}
 
 } // namespace canscope
--- canscope/device/osc_data_collecter.h
+++ canscope/device/osc_data_collecter.h
@@ -1,17 +1,25 @@
 #pragma once
 
+#include "device/register_memory.h"
+
 #include "canscope/device_errors.h"
 #include "canscope/device/data_collecter.h"
 #include "canscope/device/osc_raw_data.h"
+#include "canscope/device/register/trigger_state_register.h"
 
 namespace canscope {
+
+class UsbPort;
+class UsbPortDeviceDelegate;
 
 // implement as state machine
 class OscDataCollecter : public DataCollecter {
 public:
   typedef base::Callback<void(OscRawDataHandle)> DataCollectedCallback;
 
-  OscDataCollecter(DataCollectedCallback call_back);
+  OscDataCollecter(DataCollectedCallback call_back, 
+                   UsbPortDeviceDelegate* device_delegate, 
+                   OscDevice* osc_device);
   
   enum State {
     STATE_LOAD_CALIBRATE,
@@ -20,6 +28,8 @@ public:
   };
 
 private:
+  virtual ~OscDataCollecter() {}
+
   // implement DataCollecter
   virtual LoopState OnLoopRun() OVERRIDE;
 
@@ -27,19 +37,36 @@ private:
   
 
   device::Error DoLoop();
-  bool DoLoadCalibrate();
-  bool DoCollect();
+  void DoLoadCalibrate(LoopState* loop_state);
+  void DoCollect(LoopState* loop_state);
+  void SaveError(device::Error error);
+  void DeviceOffine();
 
-  void OnDataCollected();
+  // update property.
+  device::Error UpdateTriggerState();
+  // start scope
+  device::Error StartScope();
 
-  OscDevice* osc_device_;
+  bool IsCollected();
+
+
+  bool WriteDevice(::device::RegisterMemory* memory);
+  bool ReadDevice(::device::RegisterMemory* memory);
+  device::Error ReadData(OscRawDataHandle raw_data);
+
   State next_state_;
 
-  DeviceType type_;
-  OscRawDataDeviceConfig LastConfig();
-  UsbPort* usb_port_;
+  OscRawDataDeviceConfigHandle LastConfig();
+  OscRawDataDeviceConfigHandle last_config_;
+
+  OscDevice* osc_device_;
+  UsbPort* usb_port();
+  UsbPortDeviceDelegate* device_delegate_;
   bool stop_by_offine_;
   device::Error rv_;
+
+  TriggerStateRegister trigger_state_;
+  DataCollectedCallback call_back_;
 };
 
 } // namespace canscope
