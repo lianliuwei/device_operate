bb020eb0260bdfb89969063503ba2c7b7ff76f21
lianliuwei <lianliuwei@gmail.com>
2014-02-10 14:56:48 +0800

    add default value move DeviceType here

--- canscope/device/canscope_device_constants.h
+++ canscope/device/canscope_device_constants.h
@@ -1,6 +1,25 @@
 #pragma once
 
+// TODO move to canscope_device_util.h
+#include "base/threading/platform_thread.h"
+
 namespace canscope {
 // in us
 const int kEnumDevicesInterval = 1000;
+const int kOscCollectUpdateStateCount = 5;
+const int kOscCollectUpdateStateInterval = 20;
+const bool kDefaultAutoInitDevice = true;
+const bool kDefaultStartOnDeviceOnline = true;
+const bool kDefaultRerunOnbackOnline = true;
+
+inline void SleepMs(int64 ms) {
+  base::PlatformThread::Sleep(base::TimeDelta::FromMilliseconds(ms));
+}
+
+enum DeviceType {
+  DT_CS1201,
+  DT_CS1202,
+  DT_CS1203,
+};
+
 }; // namespace canscope
