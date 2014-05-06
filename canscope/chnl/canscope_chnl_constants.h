f478a447ca1e9f28189f7b34ac59a51a2e20061a
lianliuwei <lianliuwei@gmail.com>
2014-05-06 11:18:52 +0800

    key be const move define to cc

--- canscope/chnl/canscope_chnl_constants.h
+++ canscope/chnl/canscope_chnl_constants.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include "canscope/device/canscope_device_constants.h"
+
 namespace canscope {
 
-static void* kCANHKey = (void*)(0x01);
-static void* kCANLKey = (void*)(0x02);
-static void* kCANDIFFKey = (void*)(0x03);
+extern const void* kCANHKey;
+extern const void* kCANLKey;
+extern const void* kCANDIFFKey;
 
+const int kChnlVDiv = 8;
+const int kMaxChnlHDiv = 10; // max show h div
+
+inline int DeviceHDiv(DeviceType type) {
+  return type == DT_CS1203 ? 40 : 10;
+}
 } // namespace canscope
