7167350843734998a9a95e50df6240cb370e4988
lianliuwei <lianliuwei@gmail.com>
2014-05-06 18:06:33 +0800

    add canscope_chnl_calc

---
+++ canscope/app/canscope_chnl_calc.cc
@@ -0,0 +1,115 @@
+#include "canscope/app/canscope_chnl_calc.h"
+
+#include "canscope/chnl/canscope_chnl_constants.h"
+#include "canscope/osc_chnl_calc/can_diff_calc_item.h"
+#include "canscope/osc_chnl_calc/chnl_calc_item.h"
+
+using namespace base;
+using namespace canscope;
+
+namespace {
+static const int kDefaultFreq = 60;
+static const int kMinFreq = 10;
+static const int kFreqStep = 5;
+static const int kUIQueueMaxSize = 5;
+
+// pass ownership
+CalcGroup* BuildChnlCalcGroup() {
+  CalcGroup* chnl_group = new CalcGroup("chnl");
+  ChnlCalcItem* chnl_calc = new ChnlCalcItem("ChnlCalc", kCANHKey, kCANLKey);
+  ChnlDiffCalcItem* diff_calc = new ChnlDiffCalcItem("DiffCalc", 
+      kCANHKey, kCANLKey, kCANDIFFKey);
+  chnl_group->AddCalcItem(chnl_calc);
+  chnl_group->AddCalcItem(diff_calc);
+  chnl_group->SetDepend(kCANHKey, kCANDIFFKey);
+
+  return chnl_group;
+}
+
+}
+
+namespace canscope {
+
+CANScopeChnlCalc::CANScopeChnlCalc(scoped_refptr<base::SingleThreadTaskRunner> run_thread, 
+                                   scoped_refptr<OscRawDataQueue> raw_data_queue)
+    : run_thread_(run_thread)
+    , raw_data_queue_(raw_data_queue)
+    , running_(false)
+    , time_(kDefaultFreq) {
+  chnl_queue_ = new ChnlCalcResultQueue(false, false);
+  ui_queue_ = new ChnlCalcResultQueue(true, false);
+}
+
+void CANScopeChnlCalc::Start() {
+  run_thread_->PostTask(FROM_HERE, Bind(&CANScopeChnlCalc::StartImpl, this));
+}
+
+bool CANScopeChnlCalc::IsRunning() {
+  AutoLock lock(lock_);
+  return running_;
+}
+
+void CANScopeChnlCalc::Stop() {
+  run_thread_->PostTask(FROM_HERE, Bind(&CANScopeChnlCalc::StopImpl, this));
+}
+
+void CANScopeChnlCalc::StartImpl() {
+  DCHECK(!running_);
+  running_ = true;
+
+  raw_data_reader_.reset(new OscRawDataQueue::Reader(raw_data_queue_.get()));
+  raw_data_reader_->set_have_data_callback(Bind(&CANScopeChnlCalc::OnRawDataArrive, this));
+  raw_data_reader_->CallbackOnReady();
+  
+  chnl_calc_.reset(new ChnlCalcManager(this));
+  CalcGroup* chnl_calc = BuildChnlCalcGroup();
+  chnl_calc_->SetChnlCalc(chnl_calc);
+  // no just use same
+  chnl_calc_->SetUICalc(chnl_calc->Clone("ui"));
+}
+
+void CANScopeChnlCalc::StopImpl() {
+  DCHECK(running_);
+  running_ = false;
+  raw_data_reader_.reset(NULL);
+  chnl_calc_.reset(NULL);
+}
+
+void CANScopeChnlCalc::OnRawDataArrive() {
+  raw_data_reader_->CallbackOnReady();
+  OscRawDataHandle raw_data;
+  int64 seq;
+  bool ret = raw_data_reader_->GetResult(&raw_data, &seq);
+  DCHECK(ret);
+  chnl_calc_->RawDataCollected(raw_data);
+}
+
+bool CANScopeChnlCalc::CalcForUI() {
+  return time_.Hit();
+}
+
+void CANScopeChnlCalc::MayNotifyUI(scoped_refptr<ChnlCalcResult> result) {
+  // ui speed control
+  if (ui_queue_->bulk_num() > kUIQueueMaxSize) {
+    ui_queue_->RecycleOne();
+    double freq = time_.freq();
+    freq -= kFreqStep;
+    if (freq < kMinFreq) {
+      freq = kMinFreq;
+    }
+    time_.set_freq(freq);
+  } else if (ui_queue_->bulk_num() == 0) {
+    double freq = time_.freq();
+    freq += kFreqStep;
+    time_.set_freq(freq);
+  }
+
+  ui_queue_->PushBulk(result);
+  chnl_queue_->PushBulk(result);
+}
+
+void CANScopeChnlCalc::RecordResult(scoped_refptr<ChnlCalcResult> result) {
+  chnl_queue_->PushBulk(result);
+}
+
+} // namespace canscope
---
+++ canscope/app/canscope_chnl_calc_unittest.cc
@@ -0,0 +1,129 @@
+#include "testing/gtest/include/gtest/gtest.h"
+
+#include "canscope/app/canscope_chnl_calc.h"
+#include "canscope/device/test/scoped_open_device.h"
+#include "canscope/device/config_util.h"
+#include "canscope/device/config_manager.h"
+#include "canscope/device/osc_device/osc_device.h"
+#include "canscope/device/osc_device/osc_data_collecter.h"
+#include "canscope/device/sync_call.h"
+#include "canscope/test/test_process.h"
+#include "canscope/test/speed_meter.h"
+#include "canscope/device_errors.h"
+
+using namespace std;
+using namespace base;
+using namespace common;
+using namespace canscope;
+using namespace canscope::device;
+
+#define EXPECT_TRUE_OR_RET(ret) \
+{ \
+  EXPECT_EQ(true, (ret)); \
+  if(!(ret)) \
+  return; \
+}
+
+
+#define EXPECT_OK_OR_RET(err) \
+{ \
+  EXPECT_EQ(canscope::device::OK, (err)) << canscope::device::ErrorToString((err)); \
+  if((err) != canscope::device::OK) \
+    return; \
+}
+
+class CANScopeChnlCalcTest : public testing::Test {
+public:
+  CANScopeChnlCalcTest() 
+      : device_delegate_(CreateDeviceDelegate())
+      , open_device_(device_delegate_.get())
+    {}
+
+  virtual ~CANScopeChnlCalcTest() {}
+
+  static void SetUpTestCase() {
+    new TestProcess();
+    GetTestProcess()->Init();
+  }
+  static void TearDownTestCase() {
+    DestroyTestProcess();
+  }
+  
+protected:
+  virtual void SetUp() {
+    EXPECT_TRUE_OR_RET(open_device_.IsOpen());
+    device_delegate_->DetachFromThread();
+
+    SyncCall sync_call(CommonThread::GetMessageLoopProxyForThread(CommonThread::DEVICE));
+    sync_call.CallClosure(Bind(&CANScopeChnlCalcTest::InitDevice, Unretained(this)));
+
+    chnl_calc_ = new CANScopeChnlCalc(
+        CommonThread::GetMessageLoopProxyForThread(CommonThread::FILE),
+        osc_data_collecter_->RawDataQueue());
+  }
+
+  virtual void TearDown() {
+    EXPECT_TRUE_OR_RET(open_device_.IsOpen());
+
+    SyncCall sync_call(CommonThread::GetMessageLoopProxyForThread(CommonThread::DEVICE));
+    sync_call.CallClosure(Bind(&CANScopeChnlCalcTest::DeleteDevice, Unretained(this)));
+
+  }
+
+  void InitDevice() {
+    osc_device_.reset(new OscDevice(device_delegate_.get(), 
+        &osc_device_config_));
+    osc_device_->set_run_thread(MessageLoopProxy::current());
+    osc_device_->set_device_type(open_device_.type());
+    osc_device_config_.Update(GetDefaultOscDeviceConfig());
+    osc_device_->InitFromConfig();
+    osc_device_->Init();
+    osc_device_->SetAll();
+
+    osc_data_collecter_ = new OscDataCollecter(device_delegate_.get(), osc_device_.get());
+    osc_data_collecter_->set_run_thread(MessageLoopProxy::current());
+  }
+
+  void DeleteDevice() {
+    osc_data_collecter_->Stop();
+    osc_data_collecter_ = NULL;
+    osc_device_.reset(NULL);
+
+    // HACK or close will fault
+    device_delegate_->DetachFromThread();
+    EXPECT_OK_OR_RET(error);
+  }
+
+  scoped_refptr<CANScopeChnlCalc> chnl_calc_;
+  ConfigManager osc_device_config_;
+  scoped_ptr<OscDevice> osc_device_;
+  scoped_refptr<OscDataCollecter> osc_data_collecter_;
+  scoped_ptr<DeviceDelegate> device_delegate_;
+  ScopedOpenDevice open_device_;
+};
+
+TEST_F(CANScopeChnlCalcTest, normal) {
+  osc_data_collecter_->Start();
+  chnl_calc_->Start();
+  ChnlCalcResultQueue::Reader reader(chnl_calc_->UICalcQueue().get());
+
+  scoped_refptr<ChnlCalcResult> chnl_data;
+  int64 num;
+  SpeedMeter meter(TimeDelta::FromSeconds(1));
+  while (true) {
+    bool ret = reader.WaitGetBulk(&chnl_data, &num);
+    if (ret) {
+       //cout << "Get Raw Data"
+       //    << " detail: " << base::HexEncode(raw_data->data() + 1000, 200) << endl;
+    }
+    meter.set_size(100);
+    if (meter.DeltaPass()) {
+      cout << meter.FormatSpeedAndTotal() << endl;
+    }
+   if (meter.total_count() > 1000) {
+     break;
+   }
+  }
+
+  chnl_calc_->Stop();
+}
