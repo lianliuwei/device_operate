22e0a54a188749c0de0868f217067b9fb1235bf1
lianliuwei <lianliuwei@gmail.com>
2014-05-13 22:35:36 +0800

    update examples main_view

--- wave_control/examples/main_view.cc
+++ wave_control/examples/main_view.cc
@@ -2,64 +2,102 @@
 #include "wave_control/views/wave_control_view_factory.h"
 #include "wave_control/wave_control.h"
 #include "wave_control/examples/test_osc_wave.h"
+#include "wave_control/default_simple_ana_wave.h"
+#include "wave_control/classify_wave_control.h"
+
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 namespace {
-class TestWaveControl : public WaveControl {
+class TestWaveControl : public ClassifyWaveControl {
 public:
   virtual void AddWave(Wave* wave);
   virtual void RemoveWave(Wave* wave)  {
-    DCHECK(item_count() == 1);
-    GetItemAt(0)->RemoveWave(wave);
-  }
-  virtual void ShowInfo()  {
+    for (size_t i = 0; i < WaveContainerCount(); ++i) {
+      WaveContainer* container = GetWaveContainerAt(i);
+      YTWaveContainer* yt_container = container->AsYTWaveContainer();
+      if (yt_container->HasWave(wave)) {
+        yt_container->RemoveWave(wave);
+        return;
+      }
+    }
     NOTREACHED();
   }
-  virtual void ToggleInfo()  {
-    NOTREACHED();
-  }
-  virtual void IsShowInfo() const {
-    NOTREACHED();
-  }
+ 
+
+  void AddSimpleWave();
+
+  virtual YTWaveContainer* CreateYTWaveContainer();
 
 };
 
 class TestWaveContainer : public YTWaveContainer {
 public:
-  virtual void ShowGrid()  {
-    NOTREACHED();
-  }
-  virtual void ToggleGrid()  {
-    NOTREACHED();
-  }
-  virtual bool IsShowGrid() const {
-    NOTREACHED();
-    return true;
-  }
+
 
 };
 
 void TestWaveControl::AddWave(Wave* wave) {
-  if (item_count() == 0) {
-    Add(new TestWaveContainer());
+  for (size_t i = 0; i < item_count(); ++i) {
+    WaveContainer* container = GetWaveContainerAt(i);
+    YTWaveContainer* yt_container = container->AsYTWaveContainer();
+    if (yt_container->WaveCount() < 2) {
+      yt_container->AddWave(wave);
+      return;
+    }
   }
-  GetItemAt(0)->Add(wave);
+  YTWaveContainer* yt_container = new TestWaveContainer();
+  yt_container->AddWave(wave);
+  AddWaveContainer(yt_container);
+}
+
+YTWaveContainer* TestWaveControl::CreateYTWaveContainer() {
+  return new TestWaveContainer;
+}
+
+DefaultSimpleAnaWave* CreateAnaWave() {
+  TestAnaData* ana_data = new TestAnaData();
+  ana_data->data_range_.begin = -500;
+  ana_data->data_range_.end = 500;
+  ana_data->data_.reserve(400);
+  for (int j = 0; j < 400; j++) {
+    ana_data->data_.push_back(j * 1.0 / 400 * 500 * 1e9);
+  }
+  DefaultSimpleAnaWave* wave = new DefaultSimpleAnaWave(
+      L"ANA_1", SkColorSetRGB(255,0,255), gfx::Image(), ana_data);
+  return wave;
 }
 
 }
 
 WaveControl* CreateWaveControl() {
   TestWaveControl* wave_control = new TestWaveControl();
-  std::vector<TestOscWave*> waves  = CreateWaves();
-  for (size_t i = 0; i < waves.size(); ++i) {
-    wave_control->AddWave(waves[i]);
-  }
+   std::vector<TestOscWave*> waves  = CreateWaves();
+   for (size_t i = 0; i < waves.size(); ++i) {
+     wave_control->AddWave(waves[i]);
+   }
+  wave_control->AddWave(CreateAnaWave());
   return wave_control;
 }
+
+void SetChildBoard(views::View* const parent, int depth) {
+  if (depth == 0) {
+    return;
+  }
+  for (int i = 0; i < parent->child_count(); ++i) {
+      parent->child_at(i)->set_border(
+        views::Border::CreateSolidBorder(1, SkColorSetRGB(255, 255, 255)));
+      SetChildBoard(parent->child_at(i), depth - 1);
+  }
+}
+using namespace views;
 
 void ExampleView::Init() {
   DCHECK(contents_ == NULL) << "Run called more than once.";
   WaveControl* wave_control = CreateWaveControl();
   contents_ = WaveControlViewFactory::GetInstance()->Create(wave_control);
+ // SetChildBoard(contents_, 3);
+
   views::Widget* window =
     views::Widget::CreateWindowWithBounds(this, gfx::Rect(0, 0, 850, 400));
 
